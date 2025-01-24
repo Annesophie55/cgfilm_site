06c6920966e17ccb8344377e4cc443b0418698ba
Annesophie55 <anso.jade2013@gmail.com>
2025-01-24 12:43:38 +0100

    chore logo image file

---
+++ components/Gallery.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="gallery">
+      <div class="gallery-item">
+        <NuxtLink to="/films">
+          <NuxtImg
+            src="@/assets/images/films.webp"></NuxtImg>
+          <h2>Films</h2>
+        </NuxtLink>
+      </div>
+      <div class="gallery-item">
+        <NuxtLink to="/filmMaker">
+          <NuxtImg
+            src="@/assets/images/filmMaker.webp"></NuxtImg>
+          <h2>Le réalisateur</h2>
+        </NuxtLink>
+      </div>
+    </div> 
+</template>
